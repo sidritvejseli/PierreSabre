e9586015e0b193c6f0c9aa8ded2b18dd0542ea28
Sidrit Vejseli <vjs5009a@ensfsi.onmicrosoft.com>
2024-11-12 09:46:01 +0100

    debut traitre

---
+++ src/personnages/Traitre.java
@@ -0,0 +1,10 @@
+package personnages;
+
+public class Traitre extends Samourai {
+	int niveauTraitrise = 0;
+	public Traitre(String Seigneur, String nom, String boisson_preferee, int q_argent) {
+		super(Seigneur, nom, boisson_preferee, q_argent);
+		 
+	}
+
+}
